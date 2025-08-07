ba3f47f3f71578d5b3dc2457d6b44c8ca90a8cb6
Kumarkhatri2003 <kumarkhatri836@gmail.com>
2025-08-07 17:17:35 +0545

    updating the base templates

--- daily_journal/settings.py
+++ daily_journal/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'daily_journal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
